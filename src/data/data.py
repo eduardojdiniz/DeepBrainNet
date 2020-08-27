b68d4e11e505fd26ae33a5c32820a40d285cd25a
eduardojdiniz <edd32@pitt.edu>
2020-08-27 11:30:51 -0400

    Add to data.py same func get_preprocessed_subjects

--- src/data/data.py
+++ src/data/data.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 import sys
 import os
+import re
 
 from absl import app
 from absl import flags
@@ -15,36 +16,65 @@ FLAGS = flags.FLAGS
 
 # e.g., /home/eduardo/proj/DBN/data/raw/ADNI
 flags.DEFINE_string('data_path', None, 'Path to the data folder')
-# e.g., /home/eduardo/proj/DBN/data/raw/ADNI/subjects.txt
-flags.DEFINE_string('out_file',  None, 'Path to the output file')
+# e.g., /home/eduardo/proj/DBN/logs/ADNI/RPP
+flags.DEFINE_string('log_path',  None, 'Path to the log folder')
 
 
-def walklevel(some_path, level=1):
-    some_dir = some_path.rstrip(os.path.sep)
+def get_subdir_list(some_dir):
     assert os.path.isdir(some_dir)
-    num_sep = some_dir.count(os.path.sep)
-    for root, dirs, files in os.walk(some_dir):
-        num_sep_this = root.count(os.path.sep)
-        if num_sep + level <= num_sep_this:
-            return
-        yield (root, dirs, files)
+    (_, dirs, _) = next(os.walk(some_dir))
+    return sorted(dirs)
+
+
+def get_logged_ID_list(log_path, regex):
+    logged_ID_list = []
+    for root, dirs, files in os.walk(log_path):
+        for f in files:
+            with open(os.path.join(root, f), 'r') as my_file:
+                if re.search(regex, my_file.read()):
+                    ID = os.path.splitext(f)[0]
+                    logged_ID_list.append(ID)
+    logged_ID_list = sorted(logged_ID_list)
+    return logged_ID_list
+
+
+def save_list(my_list, out_file):
+    if os.path.exists(out_file):
+        os.remove(out_file)
+    with open(out_file, 'w+') as outFile:
+        outFile.writelines("%s\n" % item for item in my_list)
 
 
 def run_main(argv):
     del argv
-    kwargs = {'data_path': FLAGS.data_path, 'out_file': FLAGS.out_file}
+    kwargs = {'data_path': FLAGS.data_path,
+              'log_path':  FLAGS.log_path}
     main(**kwargs)
 
 
-def main(data_path, out_file):
-    # x = (root, dirs, files)
-    IDList = [x[1] for x in walklevel(data_path, level=1)]
-    # Flattening the list of IDs
-    IDList = sorted([x for ID in IDList for x in ID])
-    if not os.path.exists(out_file):
-        os.remove(out_file)
-    with open(out_file, 'w+') as outFile:
-        outFile.writelines("%s\n" % ID for ID in IDList)
+def compare_lists(first_list, second_list):
+    first_set = set(first_list)
+    second_set = set(second_list)
+    unique_to_first = sorted(list(first_set - second_set))
+    unique_to_second = sorted(list(second_set - first_set))
+    common_to_both = sorted(list(first_set & second_set))
+    return [unique_to_first, unique_to_second, common_to_both]
+
+
+def main(data_path, log_path):
+    # Get list of subject IDs in the data folder
+    ID_list = get_subdir_list(data_path)
+    # Save ID list in the output file
+    save_list(ID_list, os.path.join(data_path, "ID_list.txt"))
+    # Get ID list of RPP preprocessed subjects
+    regex = re.compile(r'\bRPP Completed\b')
+    logged_ID_list = get_logged_ID_list(log_path, regex)
+    # Save ID list of RPP preprocessed subjects
+    save_list(logged_ID_list, os.path.join(log_path, "logged_ID_list.txt"))
+    # Get list of subject IDs that were not processed
+    not_preprocessed_ID_list = compare_lists(ID_list, logged_ID_list)[0]
+    # Save ID list of RPP preprocessed subjects
+    save_list(not_preprocessed_ID_list, os.path.join(data_path, "not_processed_ID_list.txt"))
 
 
 if __name__ == '__main__':
